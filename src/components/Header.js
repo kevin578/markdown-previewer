99af5f3931278f0a613f3adb0455b8a193993c1e
kevin578 <kbriggs@aapl000894.local>
2017-12-17 21:19:57 -0500

    finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import ReactSVG from 'react-svg';
+import logo from '../img/githubicon.svg'
 
 const Header = ()=> {
     return (
         <div className = "header">
-            <h1 className = "header--title">Markdown Preview</h1>
+            <h1 className = "header--title">Markdown Previewer</h1>
+            <a href = "https://github.com/kevin578/markdown-previewer" target = "_blank" className = "header__a">
+            <object type="image/svg+xml" data={logo} className = "header--logo"/>
+            <p class = "header__p">by Kevin Briggs</p>
+            </a>
         </div>
     )
 }
